c627264e19a68ed2cc20ea4ab33e0797ffd22db7
pai-puella <cat.dyshkant@yandex.ru>
2023-10-17 12:18:33 +0300

    Added class Book

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public class Author {
+    public static class Author {
         private String firstName;
         private String lastName;
 
@@ -17,5 +17,31 @@ public class Main {
         }
     }
 
+    public class Book {
+        private String title;
+        private Author author;
+        private int year;
 
+        public Book(String title, Author author, int year) {
+            this.title = title;
+            this.author = author;
+            this.year = year;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Author getAuthor() {
+            return author;
+        }
+
+        public int getYear() {
+            return year;
+        }
+
+        public void setYear(int year) {
+            this.year = year;
+        }
+    }
 }
