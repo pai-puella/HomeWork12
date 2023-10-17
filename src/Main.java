f5ccb684c85199aa07a3776eed3de4364540b356
pai-puella <cat.dyshkant@yandex.ru>
2023-10-17 12:41:05 +0300

    Added class Library

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         }
     }
 
-    public class Book {
+    public static class Book {
         private String title;
         private Author author;
         private int year;
@@ -42,6 +42,30 @@ public class Main {
 
         public void setYear(int year) {
             this.year = year;
+        }
+    }
+
+    public static class Library {
+        public static void main(String[] args) {
+            // Создание объектов авторов
+            Author author1 = new Author("Иван", "Иванов");
+            Author author2 = new Author("Петр", "Петров");
+
+            // Создание объектов книг
+            Book book1 = new Book("Книга 1", author1, 2000);
+            Book book2 = new Book("Книга 2", author2, 2010);
+
+            // Изменение года публикации одной из книг
+            book1.setYear(2005);
+
+            // Вывод информации о книгах
+            System.out.println("Название: " + book1.getTitle());
+            System.out.println("Автор: " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName());
+            System.out.println("Год публикации: " + book1.getYear());
+
+            System.out.println("Название: " + book2.getTitle());
+            System.out.println("Автор: " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName());
+            System.out.println("Год публикации: " + book2.getYear());
         }
     }
 }
